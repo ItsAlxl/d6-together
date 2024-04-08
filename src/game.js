6f6897667171d7edc115004c62e0cd5ee27c9ece
alxl <alex.cavasso@gmail.com>
2024-04-07 20:00:09 -0500

    prompt completion is generic

--- src/game.js
+++ src/game.js
@@ -2,6 +2,9 @@ import * as DiceTray from "./dicetray.js"
 window.MY_PLAYER_ID = 1
 
 let Prompts = document.getElementById("prompt_bg")
+let PromptControls = document.getElementById("prompt_controls")
+let current_prompt = ""
+
 function generateSeed() {
   return Date.now() * Math.random()
 }
@@ -12,22 +15,36 @@ function setVisible(e, v) {
 
 DiceTray.create(document.getElementById("dice_par"))
 
-window.requestPoolRoll = function () {
-  closePrompt()
+function requestPoolRoll() {
   DiceTray.poolRoll(
     MY_PLAYER_ID,
-    { [MY_PLAYER_ID]: document.getElementById("pool_mine").valueAsNumber },
+    { [MY_PLAYER_ID]: document.getElementById("arb_mine").valueAsNumber },
     generateSeed()
   )
 }
 
-window.openPrompt = function (id) {
-  for (let i = 0; i < Prompts.children.length; i++) {
-    setVisible(Prompts.children[i], Prompts.children[i].id == id)
+window.applyPrompt = function () {
+  switch (current_prompt) {
+    case "prompt_arbitrary":
+      requestPoolRoll()
   }
-  setVisible(Prompts, true)
+  closePrompt()
+}
+
+window.openPrompt = function (id) {
+  if (id != current_prompt) {
+    current_prompt = id
+    for (let i = 0; i < Prompts.children.length; i++) {
+      setVisible(Prompts.children[i], Prompts.children[i].id == id)
+      if (Prompts.children[i].id == id) {
+        Prompts.children[i].appendChild(PromptControls)
+      }
+    }
+    setVisible(Prompts, true)
+  }
 }
 
 window.closePrompt = function () {
+  current_prompt = ""
   setVisible(Prompts, false)
 }
