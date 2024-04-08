84b47150ba554843c1e778dbe7da02c1a4624ce1
alxl <alex.cavasso@gmail.com>
2024-04-07 20:29:43 -0500

    better prompt layout

--- src/game.js
+++ src/game.js
@@ -1,8 +1,14 @@
 import * as DiceTray from "./dicetray.js"
 window.MY_PLAYER_ID = 1
 
-let Prompts = document.getElementById("prompt_bg")
-let PromptControls = document.getElementById("prompt_controls")
+const PROMPT_MAP = {
+  prompt_arbitrary: {
+    cb: requestPoolRoll,
+    title: "Build Pool",
+    act: "Roll!",
+  },
+}
+
 let current_prompt = ""
 
 function generateSeed() {
@@ -24,27 +30,32 @@ function requestPoolRoll() {
 }
 
 window.applyPrompt = function () {
-  switch (current_prompt) {
-    case "prompt_arbitrary":
-      requestPoolRoll()
+  if (PROMPT_MAP[current_prompt] != null) {
+    PROMPT_MAP[current_prompt].cb()
   }
   closePrompt()
 }
 
 window.openPrompt = function (id) {
   if (id != current_prompt) {
+    let prompts_par = document.getElementById("prompt_list")
     current_prompt = id
-    for (let i = 0; i < Prompts.children.length; i++) {
-      setVisible(Prompts.children[i], Prompts.children[i].id == id)
-      if (Prompts.children[i].id == id) {
-        Prompts.children[i].appendChild(PromptControls)
-      }
+    for (let i = 0; i < prompts_par.children.length; i++) {
+      setVisible(prompts_par.children[i], prompts_par.children[i].id == id)
     }
-    setVisible(Prompts, true)
+
+    let pdata = PROMPT_MAP[current_prompt]
+    if (pdata == null) {
+      closePrompt()
+    } else {
+      document.getElementById("prompt_title").innerText = pdata.title
+      document.getElementById("prompt_confirm_btn").innerText = pdata.act
+      setVisible(document.getElementById("prompt_bg"), true)
+    }
   }
 }
 
 window.closePrompt = function () {
   current_prompt = ""
-  setVisible(Prompts, false)
+  setVisible(document.getElementById("prompt_bg"), false)
 }
